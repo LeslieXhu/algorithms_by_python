a338ae6074a3691c098a9af6f6eb11d72e8d640f
leslie <leslie.xhu@gmail.com>
2015-08-07 11:47:48 +0800

    Add some comments for finding max subarray implementation.

--- 4_max_subarray.py
+++ 4_max_subarray.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
+"""Find a sub array which has the max sum"""
 orig_data = [13, -3, -25, 20, -3, -16, -23, 18, 20, -71, 12, -5, -22, 15, -4, 7, 39]
 print orig_data, '\n'
 
 def find_max_subarray(start, end):
+    # if the target array length is 1, return itself.
     if start + 1 == end:
         return start, end, orig_data[start]
+    # split array into 2 parts, and process them recursively.
     mid = (start + end) / 2
     left_start, left_end, left_max = find_max_subarray(start, mid)
     right_start, right_end, right_max = find_max_subarray(mid, end)
+
+    # the max subarray may crossing the middle
+    # so we seek max subarray start from middle to left and right
+    # then the sum of both sides is the max subarray acrossing middle
     mid_left_cur = orig_data[mid - 1]
     mid_left_max = mid_left_cur
     mid_start = mid - 1;
@@ -27,6 +34,8 @@ def find_max_subarray(start, end):
             mid_end = index
     mid_end += 1 # move to next as end
     mid_max = mid_right_max + mid_left_max
+
+    # compare 3 results and choose the max one.
     res_max = 0
     res_start = 0
     res_end = 0
